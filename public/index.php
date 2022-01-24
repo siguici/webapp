2742ab86e4b805c0ddc65b44223ba6c3bbedac78
SIGUI Kessé Emmanuel <developer@sikessem.com>
2022-01-25 00:25:57 +0100

    :art: Add the home page

--- public/index.php
+++ public/index.php
@@ -1,17 +1,5 @@
 <?php
 
-use Ske\IO\{Input, Output};
-use Ske\Cgi\{Server};
-
 $organizer = require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'bootstrap.php';
 
-$env_type = trim(file_get_contents(APP_ROOT . DIRECTORY_SEPARATOR . ENV_TYPE));
-$envs = parse_ini_file(APP_ROOT . DIRECTORY_SEPARATOR . ENV_FILE, true, INI_SCANNER_TYPED);
-$env = $envs[$env_type];
-
-$server = new Server($env['server.host'], $env['server.port']);
-$website = $server->addDomain($env['server.name'], APP_ROOT . '/source/main/cgi', [
-    '' => 'home'
-]);
-$website->addSubdomain('about');
-$server->process($_SERVER, new Input(fopen('php://input', 'r')), new Output(fopen("php://output", "w")));
+require (fn() => dirname(__DIR__) . '/bundle/src/main/cgi/' . ($path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?: basename(__FILE__)) . '.php')();
