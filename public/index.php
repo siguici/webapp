0eea57f725963a9fa80367c2b01bf16a3d7d8b5e
SIGUI Kessé Emmanuel <developer@sikessem.com>
2022-01-25 00:43:29 +0100

    :zap: Improve inclusion of source files

--- public/index.php
+++ public/index.php
@@ -2,4 +2,6 @@
 
 $organizer = require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'bootstrap.php';
 
-require (fn() => dirname(__DIR__) . '/bundle/src/main/cgi/' . ($path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?: basename(__FILE__)) . '.php')();
+$path = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/') ?: 'home'; 
+
+require (fn() => dirname(__DIR__) . "/bundle/src/main/cgi/$path.php")();
