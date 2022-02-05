f8bf77a65d600cf99b4712c14ec4d63930de2793
SIGUI Kessé Emmanuel <developer@sikessem.com>
2022-02-05 11:57:01 +0100

    :hammer: Add the app web page

---
+++ res/views/app.php
@@ -0,0 +1,2 @@
+<h1>Join the SIKessEm community</h1>
+<p>Design, develop and deploy your projects according to your deadline and at a lower cost or learn programming step by step according to your learning pace.</p>
---
+++ src/app/cgi/app.php
@@ -0,0 +1,4 @@
+<?= render('layout.main', [
+    'main_view' => render('app'),
+    'main_menu' => render('menu.app'),
+]);
