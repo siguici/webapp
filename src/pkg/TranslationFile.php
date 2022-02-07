76f22a85cec0e3022d0036f17d772e0d7bc490cb
SIGUI Kessé Emmanuel <developer@sikessem.com>
2022-02-07 14:17:10 +0100

    :art: Use Ske\Util\Loader\JsonLoader::loadFile() to load JSON file

--- src/pkg/TranslationFile.php
+++ src/pkg/TranslationFile.php
@@ -1,24 +1,9 @@
 <?php namespace Ske\Util;
 
+use Ske\Util\Loader\JsonLoader;
+
 class TranslationFile extends Translation {
     public function __construct(string $name, $file) {
-        if (!is_file($file)) {
-            throw new \RuntimeException("$file is not found");
-        }
-
-        if (!is_readable($file)) {
-            throw new \RuntimeException("$file is not readable");
-        }
-
-        if (!($data = @file_get_contents($file))) {
-            throw new \RuntimeException("$file is empty");
-        }
-
-        $data = json_decode($data, true);
-        if (json_last_error() !== JSON_ERROR_NONE) {
-            throw new \RuntimeException(json_last_error_msg());
-        }
-
-        parent::__construct($name, $data);
+        parent::__construct($name, JsonLoader::loadFile($file, JSON_OBJECT_AS_ARRAY));
     }
 }
