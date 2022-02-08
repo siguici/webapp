3f073fda572dd920d69fc36e69ed7244d9feb3d3
SIGUI Kessé Emmanuel <developer@sikessem.com>
2022-02-08 02:09:05 +0100

    :zap: package ske/http: implement \IteratorAggregate and \ArrayAccess from Body

--- src/pkg/Http/Body.php
+++ src/pkg/Http/Body.php
@@ -1,6 +1,6 @@
 <?php namespace Ske\Util\Http;
 
-class Body {
+class Body implements \IteratorAggregate, \ArrayAccess {
     public function __construct(string $data) {
         $this->setData($data);
     }
@@ -17,5 +17,49 @@ class Body {
             throw new \InvalidArgumentException('Invalid JSON given');
         }
         $this->data = $data;
+    }
+
+    public function getIterator(): \ArrayIterator {
+        return new \ArrayIterator($this->data);
+    }
+
+    public function offsetExists(string $offset): bool {
+        return isset($this->data[$offset]);
+    }
+
+    public function offsetGet(string $offset): mixed {
+        return $this->data[$offset];
+    }
+
+    public function offsetSet(string $offset, mixed $value): void {
+        $this->data[$offset] = $value;
+    }
+
+    public function offsetUnset(string $offset): void {
+        unset($this->data[$offset]);
+    }
+
+    public function __toString(): string {
+        return $this->getData();
+    }
+
+    public function __debugInfo(): array {
+        return $this->data;
+    }
+
+    public function __get(string $name): mixed {
+        return $this->offsetGet($name);
+    }
+
+    public function __set(string $name, mixed $value): void {
+        $this->offsetSet($name, $value);
+    }
+
+    public function __isset(string $name): bool {
+        return $this->offsetExists($name);
+    }
+
+    public function __unset(string $name): void {
+        $this->offsetUnset($name);
     }
 }
