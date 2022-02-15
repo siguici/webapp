65378a2f32de3739741477e71b686aecc922fcb4
SIGUI Kessé Emmanuel <developer@sikessem.com>
2022-02-15 11:15:18 +0100

    :hammer: view/layout: Add favicon from main

--- app/res/views/layout/main.php
+++ app/res/views/layout/main.php
@@ -5,6 +5,8 @@
         <meta charset="UTF-8"/>
         <title><?= val('app_name') ?></title>
         <meta name="description" content="<?= val('The %s Website', val('app_name')) ?>"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="shortcut icon" href="favicon.ico'" type="image/x-icon"/>
         <style>
         *::before, *, *::after {
             box-sizing: inherit;
