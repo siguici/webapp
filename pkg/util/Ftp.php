1b5dd82a5febfa07d39a4d3475c140957add63d7
SIGUI Kessé Emmanuel <developer@sikessem.com>
2022-02-17 16:18:27 +0100

    :hammer: pkg: add FTP util

---
+++ pkg/util/Ftp.php
@@ -0,0 +1,38 @@
+<?php namespace Ske\Util;
+
+class Ftp {
+    public function __construct(string $host, int $port = 21, int $timeout = 90, bool $secure = false) {
+        $secure ? $this->connectSecure($host, $port, $timeout) : $this->connect($host, $port, $timeout);
+    }
+
+    protected $connection;
+
+    public function connect(string $host, int $port = 21, int $timeout = 90): void {
+        if (!$this->connection = ftp_connect($host, $port, $timeout))
+            throw new \Exception("Failed to connect to $host:$port");
+    }
+
+    public function sslConnect(string $host, int $port = 21, int $timeout = 90): void {
+        if ($this->connection = ftp_ssl_connect($host, $port, $timeout))
+            throw new \Exception("SSL connection failure to $host:$port");
+    }
+
+    public function __call(string $name, array $arguments): mixed {
+        if (!$this->connection)
+            throw new \Exception("FTP connection not established");
+
+        if (!preg_match('/^login|pasv|chdir|cdup|chmod|delete|rename|rmdir|mkdir|nlist|pwd|systype|site|size|mdtm|chgrp|chown|get|put|rawlist|raw$/i', $name))
+            throw new \Exception("Invalid FTP command: $name");
+
+        $name = 'ftp_' . strtolower($name);
+        return $name(...$arguments);
+    }
+
+    public function close() {
+        ftp_close($this->connection);
+    }
+
+    public function __destruct() {
+        $this->close();
+    }
+}
