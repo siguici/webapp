39acef1c96b61b15acb5c7d823f5c4ac8e1dcc8a
SIGUI Kessé Emmanuel <developer@sikessem.com>
2022-02-18 01:09:53 +0100

    :art: Use Event Emitter from FTP Command-Line

--- app/src/main/cli/ftp.php
+++ app/src/main/cli/ftp.php
@@ -12,5 +12,56 @@ if (!$ftp->login(env('FTP_USERNAME'), env('FTP_PASSWORD'))) {
     exit(1);
 }
 
-$ftpClient = new FtpClient($ftp, getcwd());
-$ftpClient->run($argc, $argv);
+(new FtpClient($ftp, getcwd()))
+->on('error', function (\Throwable $e) {
+    fprintf(
+        STDERR,
+        '%s' . PHP_EOL,
+        $e->getMessage()
+    );
+})
+->on('put', function (string $remote, string $local, int $mode = FTP_ASCII) {
+    fprintf(
+        STDOUT,
+        'Uploaded %s from %s' . PHP_EOL . str_repeat("\007", 10),
+        $remote,
+        $local
+    );
+})
+->on('get', function (string $remote, string $local) {
+    fprintf(
+        STDOUT,
+        'Downloaded %s to %s' . PHP_EOL,
+        $remote,
+        $local
+    );
+})
+->on('mlsd', function (string $path) {
+    fprintf(
+        STDOUT,
+        '%s directory accessed' . PHP_EOL,
+        $path
+    );
+})
+->on('mkdir', function (string $dir) {
+    fprintf(
+        STDOUT,
+        'Created directory %s' . PHP_EOL,
+        $dir
+    );
+})
+->on('rmdir', function (string $dir) {
+    fprintf(
+        STDOUT,
+        'Removed directory %s' . PHP_EOL,
+        $dir
+    );
+})
+->on('run', function (int $argc, array $argv) {
+    fprintf(
+        STDOUT,
+        'Command %s done.' . PHP_EOL,
+        $argc > 1 ? $argv[1] : $argv[0]
+    );
+})
+->run($argc, $argv);
