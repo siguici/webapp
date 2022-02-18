7a331aeacd8274e87e08124f9b7ba14c5d84601c
SIGUI Kessé Emmanuel <developer@sikessem.com>
2022-02-18 02:24:01 +0100

    :art: pkg/util: improve module and template

--- pkg/util/Module.php
+++ pkg/util/Module.php
@@ -1,7 +1,89 @@
 <?php namespace Ske\Util;
 
 class Module {
-    public function __construct(protected string $path, protected array $data = [], protected bool $required = true) {}
+    public function __construct(string $path, array $vars = []) {
+        $this->setPath($path);
+        $this->setVars($vars);
+    }
+
+    protected string $path;
+
+    public function setPath(string $path): self {
+        if (!is_file($path)) {
+            throw new \InvalidArgumentException("$path is not a file");
+        }
+
+        $this->path = $path;
+        return $this;
+    }
+
+    public function getPath(): string {
+        return $this->path;
+    }
+
+    protected bool $required = false;
+
+    public function required(): self {
+        $this->required = true;
+        return $this;
+    }
+
+    protected bool $once = false;
+
+    public function once(): self {
+        $this->once = true;
+        return $this;
+    }
+
+    protected array $vars = [];
+
+    public function setVars(array $vars): self {
+        foreach ($vars as $key => $value) {
+            $this->setVar($key, $value);
+        }
+        return $this;
+    }
+
+    public function setVar(string $name, mixed $value): self {
+        if (!preg_match('/^[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*$/', $name)) {
+            throw new \InvalidArgumentException("$name is not a valid variable name");
+        }
+        $this->vars[$name] = $value;
+        return $this;
+    }
+
+    public function getVars(): array {
+        return $this->vars;
+    }
+
+    public function getVar(string $name): mixed {
+        return $this->vars[$name];
+    }
+
+    public function varExists(string $name): bool {
+        return isset($this->vars[$name]);
+    }
+
+    public function removeVar(string $name): self {
+        unset($this->vars[$name]);
+        return $this;
+    }
+
+    public function __set(string $name, mixed $value): void {
+        $this->setVar($name, $value);
+    }
+
+    public function __get(string $name): mixed {
+        return $this->getVar($name);
+    }
+
+    public function __isset(string $name): bool {
+        return $this->varExists($name);
+    }
+
+    public function __unset(string $name): void {
+        $this->removeVar($name);
+    }
 
     public function import(): mixed {
         if (!is_file($this->path)) {
@@ -11,15 +93,21 @@ class Module {
                 return null;
             }
         }
-        extract($this->data);
-        return $this->required ? require $this->path : include $this->path;
+        extract($this->getVars());
+        return $this->required ? ($this->once ? require_once $this->path : require $this->path) : ($this->once ? include_once $this->path : include $this->path);
     }
 
-    public function __set(string $name, mixed $value): void {
-        $this->data[$name] = $value;
+    public function render(): string {
+        ob_start();
+        $render = (string) $this->import();
+        if (is_numeric($render))
+            $render = ob_get_clean();
+        else
+            ob_end_clean();
+        return $render;
     }
 
-    public function __get(string $name): mixed {
-        return $this->data[$name];
+    public function __toString() {
+        return $this->render();
     }
 }
--- pkg/util/Template.php
+++ pkg/util/Template.php
@@ -1,17 +1,22 @@
 <?php namespace Ske\Util;
 
 class Template extends Module {
-    public function render(): string {
-        ob_start();
-        $render = (string) $this->import();
-        if (is_numeric($render))
-            $render = ob_get_clean();
-        else
-            ob_end_clean();
-        return $render;
-    }
-
-    public function __toString() {
-        return $this->render();
+    public function save(?string $path = null): string {
+        $content = file_get_contents($this->getPath());
+        if (
+            preg_match_all('/\$\{([a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*)\}/', $content, $varsNames) ||
+            preg_match_all('/\$([a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*)/', $content, $varsNames)
+        ) {
+            foreach ($varsNames[1] as $nameKey => $varName) {
+                if ($this->varExists($varName)) {
+                    throw new \RuntimeException("Missing env configuration variable ($varName)");
+                }
+                $content = str_replace($varsNames[0][$nameKey], $this->getVar($varName), $content);
+            }
+        }
+        if (isset($path)) {
+            file_put_contents($path, $content, LOCK_EX);
+        }
+        return $content;
     }
 }
