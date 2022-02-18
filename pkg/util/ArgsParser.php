376bca049de606c50c077fef47a9c7f16a824a1c
SIGUI Kessé Emmanuel <developer@sikessem.com>
2022-02-18 03:08:14 +0100

    :hammer: pkg/util: add args parser

---
+++ pkg/util/ArgsParser.php
@@ -0,0 +1,11 @@
+<?php namespace Ske\Util;
+
+class ArgsParser {
+    public function parse(int $argc, array $argv): void {
+
+    }
+
+    public function parsed(): bool {
+        return false;
+    }
+}
