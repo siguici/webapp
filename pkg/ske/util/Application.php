1b3bd269b6eaea7deaad8206068be4d72f8babd5
SIGUI Kessé Emmanuel <developer@sikessem.com>
2022-02-23 00:43:16 +0100

    :art: Use app name to process orders

--- pkg/ske/util/Application.php
+++ pkg/ske/util/Application.php
@@ -3,12 +3,26 @@
 class Application {
 	use StdStreams;
 
-	public function __construct(string $dir, array|Env $env = [], ?Input $inputStream = null, ?Output $outputStream = null, ?Output $errorStream = null) {
+	public function __construct(string $name, string $dir, array|Env $env = [], ?Input $inputStream = null, ?Output $outputStream = null, ?Output $errorStream = null) {
+		$this->setName($name);
 		$this->setDir($dir);
 		$this->setEnv($env);
 		$this->setInputStream($inputStream);
 		$this->setOutputStream($outputStream);
 		$this->setErrorStream($errorStream);
+	}
+
+	protected string $name;
+
+	public function setName(string $name) {
+		if (empty($name)) {
+			throw new \InvalidArgumentException('Name cannot be empty');
+		}
+		$this->name = $name;
+	}
+
+	public function getName(): string {
+		return $this->name;
 	}
 
 	protected string $dir;
@@ -55,15 +69,48 @@ class Application {
 			$this->getInputStream()->open('php://stdin', 'r');
 			$this->getOutputStream()->open('php://stdout', 'w');
 			$this->getErrorStream()->open('php://stderr', 'w');
-			$message = 'Run from command line' . PHP_EOL;
+
+			$argc = $args['argc'] ??= $_SERVER['argc'] ??= 0;
+			$argv = $args['argv'] ??= $_SERVER['argv'] ??= [];
+
+			if (0 === $argc) {
+				return new Result(0);
+			}
+
+			$name = basename($argv[0]);
+			if ($name === $this->getName()) {
+				array_shift($argv);
+				--$argc;
+			}
+
+			return $this->processCommand($name, $argc, $argv);
 		}
 		else {
 			$this->getInputStream()->open('php://input', 'r');
 			$this->getOutputStream()->open('php://output', 'w');
 			$this->getErrorStream()->open('php://output', 'w');
-			$message = 'Run from web <br/>';
+
+			$name = $args['SERVER_NAME'] ??= $_SERVER['SERVER_NAME'] ??= 'localhost';
+			$host = $args['HTTP_HOST'] ??= $_SERVER['HTTP_HOST'] ??= '127.0.0.1';
+			$port = $args['SERVER_PORT'] ??= $_SERVER['SERVER_PORT'] ??= 80;
+			$username = $args['PHP_AUTH_USER'] ??= $_SERVER['PHP_AUTH_USER'] ??= '';
+			$password = $args['PHP_AUTH_PW'] ??= $_SERVER['PHP_AUTH_PW'] ??= '';
+			$scheme = $args['REQUEST_SCHEME'] ??= $_SERVER['REQUEST_SCHEME'] ??= 'http';
+			$method = $args['REQUEST_METHOD'] ??= $_SERVER['REQUEST_METHOD'] ??= 'GET';
+			$uri = $args['REQUEST_URI'] ??= $_SERVER['REQUEST_URI'] ??= '/';
+
+			$url = $scheme . '://' . ($username ? $username . ':' . $password . '@' : '') . $host . ($port ? ':' . $port : '') . $uri;
+			return $this->processRequest($name, $method, $url);
 		}
 		return new Result(0, $message);
+	}
+
+	public function processCommand(string $name, int $argc, array $argv): Result {
+		return new Result(0, "Command $name is running in CLI mode with $argc arguments: " . implode(' ', $argv) . PHP_EOL);
+	}
+
+	public function processRequest(string $name, string $method, string $url): Result {
+		return new Result(0, "<p>Request $name is running in CGI mode with HTTP $method to $url</p>");
 	}
 
 	public function write(Result $result): int|false {
--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@ use Ske\Util\Application;
 
 require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'bootstrap.php';
 
-$app = new Application(dirname(__DIR__) . '/app/src/main/cgi', env());
+$app = new Application(env('WEB_HOST', 'localhost'), dirname(__DIR__) . '/app/src/main/cgi', env());
 
 $result = $app->run($_SERVER);
 
