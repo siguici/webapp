273371ba017988bf38e1b9a0edea0fe619b1e164
SIGUI Kessé Emmanuel <developer@sikessem.com>
2022-02-22 13:55:07 +0100

    :zap: pkg: improve Dotignore util file

--- pkg/ske/util/Dotignore.php
+++ pkg/ske/util/Dotignore.php
@@ -1,110 +1,34 @@
 <?php namespace Ske\Util;
 
-class Dotignore implements \IteratorAggregate, \Countable, \ArrayAccess {
-    public function __construct(array $patterns = []) {
-        $this->setPatterns($patterns);
+/**
+ * Class Dotignore - A class to handle .*ignore files
+ *
+ * @package ske/util
+ * @author  SIGUI Kessé Emmanuel <developer@sikessem.com>
+ * @link    https://pkg.sikessem.com/ske/util/Dotignore
+ * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+class Dotignore extends Ignore {
+    public function __construct(string $file) {
+        parent::__construct(self::load($file));
+        $this->file = $file;
     }
 
-    protected array $patterns = [];
+    protected string $file;
 
-    public function setPatterns(array $patterns) {
-        $this->patterns = [];
-        foreach ($patterns as $pattern) {
-            $this->addPattern($pattern);
-        }
+    public function getFile(): string {
+        return $this->file;
     }
 
-    public function addPatterns(array $patterns) {
-        foreach ($patterns as $pattern) {
-            $this->addPattern($pattern);
-        }
-    }
-
-    public function addPattern($pattern) {
-        $this->patterns[] = $pattern;
-    }
-
-    public function getPatterns(): array {
-        return $this->patterns;
-    }
-
-    public function isIgnored(string $name): bool {
-        $name = str_replace('\\', '/', $name);
-        $name = rtrim($name, '/');
-        $ignored = false;
-        foreach ($this->getPatterns() as $pattern) {
-            if (str_starts_with($pattern, '#')) {
-                continue;
-            }
-            if (str_starts_with($pattern, '!')) {
-                if ($this->match(substr($pattern, 1), $name)) {
-                    $ignored = false;
-                }
-                continue;
-            }
-            if (str_starts_with($pattern, '/')) {
-                if ($this->match($pattern, $name))
-                    $ignored = true;
-                continue;
-            }
-            if ($this->match($pattern, basename($name)))
-                $ignored = true;
-        }
-        return $ignored;
-    }
-
-    public static function match(string $pattern, string $name): bool {
-        if (str_ends_with($pattern, '/')) {
-            return self::match(substr($pattern, 0, -1), $name) || self::match($pattern . '*', $name);
-        }
-        return fnmatch($pattern, $name);
-    }
-
-    public function loadFile(string $file): self {
-        if (empty($file)) {
-            throw new \InvalidArgumentException('File name cannot be empty');
-        }
-
+    public static function load(string $file): array {
         if (!\is_file($file)) {
-            throw new \InvalidArgumentException("$file does not exist");
+            throw new \InvalidArgumentException("$file is not a file");
         }
 
         if (!\is_readable($file)) {
             throw new \InvalidArgumentException("$file is not readable");
         }
-        $this->setPatterns(\file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES));
-        return $this;
-    }
 
-    public function getIterator(): \Traversable {
-        return new \ArrayIterator($this->patterns);
-    }
-
-    public function count(): int {
-        return \count($this->patterns);
-    }
-
-    public function offsetExists(mixed $offset): bool {
-        return isset($this->patterns[$offset]);
-    }
-
-    public function offsetGet(mixed $offset): mixed {
-        return $this->patterns[$offset];
-    }
-
-    public function offsetSet(mixed $offset, mixed $value): void {
-        $this->patterns[$offset] = $value;
-    }
-
-    public function offsetUnset(mixed $offset): void {
-        unset($this->patterns[$offset]);
-    }
-
-    public function __toString(): string {
-        return implode(PHP_EOL, $this->patterns);
-    }
-
-    public function __debugInfo(): array {
-        return $this->patterns;
+        return \file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
     }
 }
